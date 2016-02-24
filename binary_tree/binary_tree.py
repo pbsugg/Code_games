86f222c9f3cfeff5b6d2ed5d452ce153e135dfa1
Philip Sugg <pbensugg@gmail.com>
2016-02-23 23:27:00 -0600

    working binary node with tests

---
+++ binary_tree/binary_tree.py
@@ -0,0 +1,6 @@
+from node import Node
+
+class BinaryTree:
+
+    def __init__(self, root):
+        
--- binary_tree/binary_tree_test.py
+++ binary_tree/binary_tree_test.py
@@ -1,11 +1,25 @@
 import unittest
+from node import Node
 
 class TestNode(unittest.TestCase):
     def setUp(self):
-        self.node = Node()
+        self.node = Node(1)
 
 
+    def test_value(self):
+        self.assertEqual(self.node.value, 1)
 
+    def test_children(self):
+        self.node.left_child = Node(2)
+        self.node.right_child = Node(3)
+        self.assertIsInstance(self.node.left_child, Node)
+        self.assertIsInstance(self.node.right_child, Node)
+        self.assertEqual(self.node.left_child.value, 2)
+        self.assertEqual(self.node.right_child.value, 3)
+
+
+class TestBinaryTree(unittest.TestCase):
+    
 
 if __name__ == '__main__':
     unittest.main()
