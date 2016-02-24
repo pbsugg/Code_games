10b64d3ad7d8da136863eea421f67f6282beb8d9
Philip Sugg <pbensugg@gmail.com>
2016-02-24 12:46:30 -0600

    add skeleton of tests for in post and pre-order traversal

--- binary_tree/binary_tree_test.py
+++ binary_tree/binary_tree_test.py
@@ -61,8 +61,27 @@ class BinaryTreeTestCase(unittest.TestCase):
         node6 = Node(6)
         self.binary_tree.insert(root, node6)
         self.assertEqual(root.right_child.right_child.left_child, node6)
+        node7 = Node(7)
+        self.binary_tree.insert(root, node7)
+        self.assertEqual(root.right_child.right_child.right_child, node7)
 
 
+    def test_visit_in_order(self):
+        root = self.binary_tree.root
+        for node in range(2,7):
+            node = Node(node)
+            self.binary_tree.insert(root, node)
+        self.assertEqual(self.binary_tree.visitInOrder(root), [4, 2, 5, 1, 6, 3, 7])
+
+
+    def test_visit_pre_order(self):
+        root = self.binary_tree.root
+        for node in range(2,15):
+            node = Node(node)
+            self.binary_tree.insert(root, node)
+
+    def test_visit_post_order(self):
+        pass
 
 
 if __name__ == '__main__':
