1991456f8b27a15fdfba3a321c04919cfaefad02
Philip Sugg <pbensugg@gmail.com>
2016-02-24 00:32:36 -0600

    add working tests for binary tree insert

--- binary_tree/binary_tree.py
+++ binary_tree/binary_tree.py
@@ -3,4 +3,12 @@ from node import Node
 class BinaryTree:
 
     def __init__(self, root):
-        
+        self.root = root
+
+    def insert(self, node_to_insert):
+        if self.root.left_child == None:
+            self.root.left_child = node_to_insert
+        elif self.root.right_child == None:
+            self.root.right_child = node_to_insert
+        else:
+            raise SpaceError("No Space!")
--- binary_tree/binary_tree_test.py
+++ binary_tree/binary_tree_test.py
@@ -1,37 +1,25 @@
 import unittest
 from node import Node
+from binary_tree import BinaryTree
 
-class TestNode(unittest.TestCase):
-    def setUp(self):
-        self.node = Node(1)
-
-    def test_value(self):
-        self.assertEqual(self.node.value, 1)
-
-    def test_children(self):
-        self.node.left_child = Node(2)
-        self.node.right_child = Node(3)
-        self.assertIsInstance(self.node.left_child, Node)
-        self.assertIsInstance(self.node.right_child, Node)
-        self.assertEqual(self.node.left_child.value, 2)
-        self.assertEqual(self.node.right_child.value, 3)
-
-
-class TestBinaryTree(unittest.TestCase):
+class BinaryTree(unittest.TestCase):
 
     def setUp(self):
         rootNode = Node(1)
-        self.root = rootNode
+        self.tree = BinaryTree(rootNode)
 
-    def test_has_root(self):
-        self.assertEqual(self.root, rootNode)
+    def test_root(self):
+        self.assertEqual(self.tree.root, rootNode)
 
     def test_insert(self):
         node2 = Node(2)
         node3 = Node(3)
-        self.insert(node2)
-        self.insert(node3)
-        self.assertEqual(self.root.left_child, node2)                self.assertEqual(self.root.right_child, node3)
+        self.tree.insert(node2)
+        self.tree.insert(node3)
+        self.assertEqual(self.root.left_child, node2)
+        self.assertEqual(self.root.right_child, node3)
+        anotherNode = Node(4)
+        self.assertRaises(self.tree.insert(anotherNode), SpaceException)
 
 
 if __name__ == '__main__':
