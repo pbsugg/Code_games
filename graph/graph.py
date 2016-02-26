f819882fe183c50b45a10c1d33f918bf2fec533b
Philip Sugg <pbensugg@gmail.com>
2016-02-26 11:18:47 -0600

    add flag and graph method for visiting nodes

--- graph/graph.py
+++ graph/graph.py
@@ -6,3 +6,9 @@ class Graph:
     def __init__(self, nodes = []):
         self.nodes = nodes
         self.search_output = []
+
+    def visit(self, node):
+        node.visited = True
+
+    def depth_first_search(self, root):
+        
--- graph/node_graph.py
+++ graph/node_graph.py
@@ -4,6 +4,8 @@ class NodeGraph:
     def __init__(self, name, children = []):
         self.name = name
         self.children = children
+        # "visit" flag for searches
+        self.visited = False
 
 
         def add_child(self, new_child):
