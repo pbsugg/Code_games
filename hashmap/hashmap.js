44713c36a0259bdff97a31a7e35e7c8713cfe30b
Philip Sugg <pbensugg@gmail.com>
2016-03-14 10:37:31 -0500

    reconfigure hashTable object with: prototypes

--- hashmap/hashmap.js
+++ hashmap/hashmap.js
@@ -9,7 +9,7 @@ Inputs (attributes):
 (1) Integer: buckets
 (2) "Buckets" (number of spaces in table)
 (3) Hashing function (for creating the hash address)
-(4) "Match"--for determining whether two keys match(input: two keys)
+(4) "Match"--for determining whether two keys match(input: two keys) (lookup function)
 (5) "Destroy" function-- eliminate data
 (5) "Table" --the array of buckets (linked lists)
 
@@ -23,13 +23,7 @@ Inputs (attributes):
  * get unicode value of char at that position
  * return key value
  */
-
-var getKey = function(itemToAdd){
-	stringified = itemToAdd.toString()
-	randomStringPosition = Math.floor(Math.random() * stringified.length)
-	unicodeKey = stringified.charCodeAt(randomStringPosition)
-	return(unicodeKey)
-}
+//
 // hashing function
 /*pseudo
  * input: key value
@@ -38,18 +32,18 @@ var getKey = function(itemToAdd){
  * return bucket value
  */
 
-var hasher = function(key, buckets){
-	hashValue = key % buckets
-	return(hashValue)
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 
-var buckets = function(){
-   return("hello")
- }
 
-var match = function(key1, key2){
+
+function hashTable(buckets) {
+
+  this.buckets = buckets,
+  this.size = function(){
+  }
+  this.destroy = destroy,
+  this.table = []
 
 }
 
@@ -57,19 +51,29 @@ var destroy = function(key){
 
 }
 
-function hashTable(buckets) {
+hashTable.prototype.hasher = function(key, buckets){
+	hashValue = key % buckets
+	return(hashValue)
+}
 
-  this.buckets = buckets,
-  this.size = function(){
+hashTable.prototype.getKey = function(itemToAdd){
+	stringified = itemToAdd.toString()
+	randomStringPosition = Math.floor(Math.random() * stringified.length)
+	unicodeKey = stringified.charCodeAt(randomStringPosition)
+	return(unicodeKey)
+}
 
-  }
-  this.computeHash = function(itemToAdd){
-  	var key = getKey(itemToAdd)
-	var hash = hasher(key, this.buckets)
+hashTable.prototype.computeHash = function(itemToAdd){
+  	var key = this.getKey(itemToAdd)
+	var hash = this.hasher(key, this.buckets)
 	return(hash)
   }
-  this.match= match
-  this.destroy = destroy,
-  this.table = []
 
+hashTable.prototype.match = function(key1, key2){
+	if(key1 == key2){
+	    return true
+	}
+	else{
+	    return false
+	}
 }
--- hashmap/test_hashmap.js
+++ hashmap/test_hashmap.js
@@ -1,12 +1,15 @@
 describe('key-deriving function', function(){
+    	beforeEach(function(){
+		this.testHashTable = new hashTable(10)	
+	})
 	it("returns a key for a given string value", function(){
 			
 		var item1 = [1,2,3,4]	
 		var item2 = "dsadasdas"
 		var item3 = 345122
-		expect(getKey(item1)).toMatch(/[0-9]+/)
-		expect(getKey(item2)).toMatch(/[0-9]+/)
-		expect(getKey(item3)).toMatch(/[0-9]+/)
+		expect(this.testHashTable.getKey(item1)).toMatch(/[0-9]+/)
+		expect(this.testHashTable.getKey(item2)).toMatch(/[0-9]+/)
+		expect(this.testHashTable.getKey(item3)).toMatch(/[0-9]+/)
 	
 	})
 
@@ -14,13 +17,16 @@ describe('key-deriving function', function(){
 
 describe("hashFunction", function(){
 
+    	beforeEach(function(){
+		this.testHashTable = new hashTable(10)	
+	})
 	it("computes the hash value based on buckets", function(){
 		var buckets = 10
 		var first = 3, second = 235, third = 0, fourth = 37	
-		expect(hasher(first, buckets)).toBe(3)
-		expect(hasher(second, buckets)).toBe(5)
-		expect(hasher(third, buckets)).toBe(0)
-		expect(hasher(fourth, buckets)).toBe(7)
+		expect(this.testHashTable.hasher(first, buckets)).toBe(3)
+		expect(this.testHashTable.hasher(second, buckets)).toBe(5)
+		expect(this.testHashTable.hasher(third, buckets)).toBe(0)
+		expect(this.testHashTable.hasher(fourth, buckets)).toBe(7)
 	   
 	})
 
@@ -38,13 +44,12 @@ describe("hashTable", function(){
 	})
 
 	it("initializes a new hashTable with required buckets", function(){
-	
-	
 		expect(self.testHashTable.buckets).toBe(10)
-	
-	
 	})
 
-		
+	it("can perform a lookup if a current value is in the hash", function(){
+		expect(self.testHashTable.match(33, 45)).toBe(false)
+		expect(self.testHashTable.match(33, 33)).toBe(true)
+	})
 
 })
