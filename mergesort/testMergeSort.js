1e6d4293f2a76880ae74faae31acdbea5351f6e7
Philip Sugg <pbensugg@gmail.com>
2016-03-18 12:09:13 -0500

    add tests adapted from quickSort

---
+++ mergesort/testMergeSort.js
@@ -0,0 +1,46 @@
+describe("mergeSort an array of numbers", function(){
+
+    	beforeEach(function(){
+	
+		this.comparison_basis = function compareNumbers(a,b){                            
+			return a - b	
+		}	
+	})
+bob = function that is the best(hello)		
+	//have to structure tests where I duplicate array and then compare and
+	//(destructively modified) original because mergeSort does not return
+	//array
+	it("can deal with arrays where there are no repeating values", function(){
+		var test_array = [5, 29, 100, 0, 15, 76, 3000, 12, 75, 800, 1021, 18]	
+		var arrayDuplicate = test_array
+		mergeSort(test_array, 0, (test_array.length - 1), comparisonOperator)	
+
+		expect(test_array).toEqual(arrayDuplicate.sort(this.comparison_basis))
+	})
+
+	it("can deal with duplicate values", function(){
+		var test_array = [5, 100, 100, 0, 15, 76, 3000, 12, 75, 75, 1021, 18, 76]	
+		var arrayDuplicate = test_array
+		mergeSort(test_array, 0, (test_array.length - 1), comparisonOperator)	
+	
+		expect(test_array).toEqual(arrayDuplicate.sort(this.comparison_basis))
+	
+	})
+
+	it("can deal with a very large number of values", function(){
+		var test_array = []
+	    	for(var i = 0; i <= 10000; i++){
+		// Generate value between 0 and 1000: 10,000 times
+	   		test_array[i] = Math.floor(Math.random() * 1000)
+	    	}
+
+		var arrayDuplicate = test_array
+		mergeSort(test_array, 0, (test_array.length - 1), comparisonOperator)	
+
+		expect(test_array).toEqual(arrayDuplicate.sort(this.comparison_basis))
+	})
+
+
+
+
+})
