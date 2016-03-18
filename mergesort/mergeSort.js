3b010ffb7c9064311ccd7df79ebeeb1ad99a3f27
Philip Sugg <pbensugg@gmail.com>
2016-03-18 12:15:47 -0500

    add basic shell of mergeSort method with comparisonOperator function from quickSort

---
+++ mergesort/mergeSort.js
@@ -0,0 +1,19 @@
+
+
+var mergeSort = function(arrayToSort, lowerBound, upperBound, comparisonOperator){
+
+})
+
+
+var comparisonOperator = function(value1, value2){
+    
+	if(value1 > value2){
+	    return 1
+	}   
+	else if (value2 > value1){
+	    return -1
+	}
+	else{
+	    return 0
+	}
+}
