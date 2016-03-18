cf103c54e09a210fe1fa407eb85e8ee0f8071370
Philip Sugg <pbensugg@gmail.com>
2016-03-18 15:11:41 -0500

    add mergeSort function with recursive calls, still need to finishing writing the merge function

--- mergesort/mergeSort.js
+++ mergesort/mergeSort.js
@@ -1,9 +1,53 @@
-
+/*
+ *pseudo
+ Inputs: 
+ *Array
+ * Upper and Lower bound indexes (start by set to 0 and (length -1))
+ * comparison basis function (taken from quicksort)
+ * 
+ * Functions:
+ * splitElements: keep splitting until you have individual elements
+ * MergeSort: recombine elements, comparing at every stage
+ * comparisonOperator: function that provides basis for comparison
+ */
 
 var mergeSort = function(arrayToSort, lowerBound, upperBound, comparisonOperator){
 
+	//calculate midpoint
+	if (lowerBound < upperBound){
+
+		var midpoint = ((lowerBound + upperBound) / 2) 
+
+		if(mergeSort(arrayToSort, lowerBound, midPoint, comparisonOperator) < 0){
+
+			return -1
+		}
+		if(mergeSort(arrayToSort, lowerBound, upperBound, comparisonOperator) < 0){
+
+			return -1
+		}
+
+		if(merge(arrayToSort, lowerBound, midpoint, upperBound, comparisonOperator) < 0){
+			return -1
+		}
+	}
+
 })
 
+var merge = function(arrayToSort, lowerBound, midPoint, upperBound, comparisonOperator){
+
+	var lowerBoundPosition = lowerBound;
+	var midPointPosition = midPoint + 1;
+	var mPos = 0;
+	while(lowerBoundPosition <= midPoint ||  midPointPosition <= upperBound){
+	
+		if(lowerPosition <= midPoint){
+			while(midPointPosition <= upperBound){
+
+			}
+		}
+	}
+})
 
 var comparisonOperator = function(value1, value2){
     
