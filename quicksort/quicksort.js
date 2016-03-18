0cded249f3cfd40e7bb44b922cb29dc4b54b5ef5
Philip Sugg <pbensugg@gmail.com>
2016-03-17 22:46:00 -0500

    add provisional working solution for quicksort

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -26,13 +26,54 @@
 
 var quickSort = function(arrayToSort, startPoint, endPoint, comparisonOperator){
 
+    if(startPoint < endPoint){
+    
+    var pivot = partition(arrayToSort, startPoint, endPoint, comparisonOperator)
+    //sort right and left side partitions recursively
+    quickSort(arrayToSort, startPoint, pivot - 1, comparisonOperator)
+    quickSort(arrayToSort, pivot + 1, endPoint, comparisonOperator)
+   
+    }
 
 }
 
+//partition into two sorted arrays
+var partition = function(arrayToSort, startPoint, endPoint, comparisonOperator){
 
-var partition = function(arrayToSort){
+    var pivot = medianOfThree(arrayToSort)
 
-	var pivot = medianOfThree(arrayToSort)
+   --startPoint;
+   ++endPoint;
+   var pivot = medianOfThree(arrayToSort);
+
+    do{
+	//checking to see if values are on right side of their pivot
+	do{
+		
+		--endPoint;
+
+	   }while(comparisonOperator(endPoint, pivot) > 0)
+
+	do{
+	    	++startPoint;
+	
+	   }while(comparisonOperator(startPoint, pivot) < 0)
+
+	if(startPoint >= endPoint){
+
+		break	
+	}
+	else{
+	    // switch the values whenver one is on wrong side of pivot
+	    var holder = arrayToSort[startPoint]
+	    arrayToSort[startPoint] = arrayToSort[endPoint]
+	    arrayToSort[endPoint] = holder
+	}
+   //not expecting the conditional to break out--waiting for "break" to trigger
+    } while(1)
+
+    return pivot;
+
 }
 
 
@@ -40,31 +81,31 @@ var partition = function(arrayToSort){
 
 
 var comparisonOperator = function(value1, value2){
-	
+    
 	if(value1 > value2){
-		return 1;	
-	}
-
-	else if(value2 > value1){
-		return -1;	
+	    return 1
+	}   
+	else if (value2 > value1){
+	    return -1
 	}
 	else{
-		return 0;	
+	    return 0
 	}
-
 }
 
 var insertionSort = function(arrayToSort){
 
-//Building a quick insertion sort algorithm to help derive median
+//Building a quick insertion sort algorithm for the hell of it and to help derive median
 // worse-case O complexity n^2
   	 
+	    //keep shifting elements one over to the right until you find
+	    // this current is not less than the value to left of current 
+	    // This has nice side effect of deleting current's original
+	    // position, don't have to worry about indexing out of your array
+
     for(var current = 1; current < arrayToSort.length; current++){
 
 	var nextToSort = current 
-	    //keep shifting elements one over to the right until you find
-	    // correct sorted position
-	    // comparing current with the element to the left of current
 	while( current > 0 && (arrayToSort[current] < arrayToSort[current - 1])){
 	    	var holder = arrayToSort[current] 
 		arrayToSort[current] = arrayToSort[current - 1]	
@@ -76,11 +117,14 @@ var insertionSort = function(arrayToSort){
 }
 
 //need this for my median of three method
+//Select three elements from array at random
+//get the middle (median) value of those three
 var medianOfThree = function(arrayOfNumbers){
 	//select three random values
 	var random_values = []
 	for (var i = 0; i < 3; i++){
-		var randomFromArray = Math.floor(Math.random() * (arrayOfNumbers.length))
+		var randomFromArray = 
+		Math.floor(Math.random() * (arrayOfNumbers.length))
 		random_values[i] = arrayOfNumbers[randomFromArray]
 	}
 	var sorted  = insertionSort(random_values)
