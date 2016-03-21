c7cd2a23e1c885806337fbf82ab3ca888bbd0dfa
Philip Sugg <pbensugg@gmail.com>
2016-03-21 12:22:56 -0500

    add another solution for mergeSort

--- mergesort/testMergeSort.js
+++ mergesort/testMergeSort.js
@@ -15,19 +15,20 @@ describe("mergeSort an array of numbers", function(){
 		for(var i = 0; i < test_array.length; i++){
 			arrayDuplicate[i] = test_array[i];
 		}
-		mergeSort(test_array, 0, (test_array.length - 1), comparisonOperator)	
-		console.log(test_array)
-		var sortedDuplicate = arrayDuplicate.sort(this.comparison_basis) 
-		console.log(sortedDuplicate)
-		console.log(test_array)
+		var resultArray = []
+		mergeSort(test_array, 0, (test_array.length - 1), resultArray)	
 
-		expect(test_array).toEqual(sortedDuplicate)
+		expect(test_array).toEqual(arrayDuplicate.sort(this.comparison_basis))
 	})
 
 	it("can deal with duplicate values", function(){
 		var test_array = [5, 100, 100, 0, 15, 76, 3000, 12, 75, 75, 1021, 18, 76]	
-		var arrayDuplicate = test_array
-		mergeSort(test_array, 0, (test_array.length - 1), comparisonOperator)	
+		var arrayDuplicate = []
+		for(var i = 0; i < test_array.length; i++){
+			arrayDuplicate[i] = test_array[i];
+		}
+		var resultArray = []
+		mergeSort(test_array, 0, (test_array.length - 1), resultArray)	
 	
 		expect(test_array).toEqual(arrayDuplicate.sort(this.comparison_basis))
 	
@@ -39,9 +40,12 @@ describe("mergeSort an array of numbers", function(){
 		// Generate value between 0 and 1000: 10,000 times
 	   		test_array[i] = Math.floor(Math.random() * 1000)
 	    	}
-
-		var arrayDuplicate = test_array
-		mergeSort(test_array, 0, (test_array.length - 1), comparisonOperator)	
+		var arrayDuplicate = []
+		for(var i = 0; i < test_array.length; i++){
+			arrayDuplicate[i] = test_array[i];
+		}
+		var resultArray = []
+		mergeSort(test_array, 0, (test_array.length - 1), resultArray);	
 
 		expect(test_array).toEqual(arrayDuplicate.sort(this.comparison_basis))
 	})
